342d84c5ee1863ca812d6a0fb18226cf905216fa
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-04-04 10:06:16 +0530

    commmit to master

--- src/test/java/sampleFlipkart/SampleTest.java
+++ src/test/java/sampleFlipkart/SampleTest.java
@@ -6,6 +6,8 @@ public class SampleTest {
 
 	@Test
 	public void testScript() {
+		System.out.println("hi");
+		System.out.println("welcome");
 		System.out.println("----Testing----");
 	}
 }
---
+++ src/test/java/sampleFlipkart/SampleTest1.java
@@ -0,0 +1,12 @@
+package sampleFlipkart;
+
+import org.testng.annotations.Test;
+
+public class SampleTest1 {
+	@Test
+	public void test1()
+	{
+		System.out.println("test1 from sampe test1");
+	}
+
+}
