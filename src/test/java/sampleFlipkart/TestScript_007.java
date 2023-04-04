84bd85e7753b210d4008709a2580dd5471d25253
USER <USER@DESKTOP-FUGEFD6>
2023-04-04 10:04:56 +0530

    commit

---
+++ src/test/java/sampleFlipkart/TestScript_007.java
@@ -0,0 +1,11 @@
+package sampleFlipkart;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestScript_007 {
+@Test
+public void jamesBond() {
+	Reporter.log("JamesBond 007");
+}
+}
